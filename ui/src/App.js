b3763855be1695f1123a3f3f7a052875819300be
Vincent Belfoure <vbelfoure@hotmail.com>
2020-04-12 13:00:42 -0600

    Updated navigation

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainNav from './components/layout/MainNav'
-// import NavBar from './components/layout/NavBar';
 import Dashboard from './components/dashboard/Dashboard'
 import SupplyDashboard from './components/dashboard/SupplyDashboard'
 import MovieDashboard from './components/dashboard/MovieDashboard'
 import FoodDashboard from './components/dashboard/FoodDashboard'
 import AboutUsDashboard from './components/dashboard/AboutUsDashboard'
+import Login from './components/login/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Geocode from "react-geocode";
 import { connect } from 'react-redux';
 import { getLocation } from './store/actions/getLocationActions'
-//import { ThemeProvider } from 'react-bootstrap';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 
@@ -35,6 +33,7 @@ class App extends Component {
               <Route path="/movies" component={MovieDashboard} />
               <Route path="/foods" component={FoodDashboard} />
               <Route path="/aboutus" component={AboutUsDashboard} />
+              <Route path='/login' component={Login} />
             </Switch>
           </div>
         </BrowserRouter>
